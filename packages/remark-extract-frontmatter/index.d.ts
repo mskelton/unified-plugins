7d457eda5b1a172025ffbea91aee0d148933e770
Mark Skelton <mdskelton99@gmail.com>
2023-10-14 22:50:24 -0500

    feat: Add remark-extract-frontmatter

---
+++ packages/remark-extract-frontmatter/index.d.ts
@@ -0,0 +1,5 @@
+import type { Root } from "mdast";
+import type { Plugin } from "unified";
+
+declare const remarkExtractFrontmatter: Plugin<unknown[], Root, string>;
+export default remarkExtractFrontmatter;
---
+++ packages/remark-extract-frontmatter/index.js
@@ -0,0 +1,7 @@
+import { matter } from "vfile-matter";
+
+export default function remarkExtractFrontmatter() {
+  return function (_, file) {
+    matter(file);
+  };
+}
